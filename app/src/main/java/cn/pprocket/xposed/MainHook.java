977477931fa1cd8118e2097ff3faa0ab93a4c865
114514ns <lua6705@gmail.com>
2023-11-04 22:12:28 +0800

    Initial commit

---
+++ app/src/main/java/cn/pprocket/xposed/MainHook.java
@@ -0,0 +1,12 @@
+package cn.pprocket.xposed;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+public class MainHook implements IXposedHookLoadPackage {
+
+    @Override
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam param) throws Throwable {
+
+    }
+}
